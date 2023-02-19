effa5e2dd30d39c1958b7ee2ca013f8d657aae5f
MartinKrusteff <75111161+MartinKrusteff@users.noreply.github.com>
2023-02-19 21:09:14 +0200

    Update

--- src/main/java/com/example/MobileMK/controller/controller.java
+++ src/main/java/com/example/MobileMK/controller/controller.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 @Controller
 public class controller {
@@ -23,6 +25,15 @@ public class controller {
     @GetMapping("/")
     public String viewMainPage(Model model) {
         return "MainPage";
+    }
+    @GetMapping("/AddProblem")
+    public String moviePage() {
+        return "AddProblem";
+    }
+    @PostMapping("/AddProblem")
+    public String showMovieForm(BrandEntity brandEntity) {
+        BrandRepo.saveAndFlush(brandEntity);
+        return "AddProblem";
     }
 
 }
